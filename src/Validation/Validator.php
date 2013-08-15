356028ef61dd1706164dd818b2016baaca5026a9
Florin Miu <florin_miu@yahoo.com>
2013-08-16 00:24:23 +0300

    Added the API for the Validator

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -15,13 +15,40 @@ class Validator {
 		if (is_array($rules) or
 			$rules instanceOf Traversable) {
 			foreach ($rules as $rule) {
-
+				call_user_func_array(array($this, 'add'), $rule);
 			}
 		}
 	}
 
 	protected function compileMessage($message) {
+		if (is_array($message)) {
+			return call_user_func_array('sprintf', $message);
+		}
+		return $message;
+	}
 
+	protected function getRuleId($rule) {
+		$ruleId = $rule['name'];
+		// certain rules can be applied multiple times with different parameters
+		switch ($rule['name']) {
+			case 'regex':
+			case 'notRegex':
+				$ruleId .= $rule['params'][0];
+			break;
+			case 'callback':
+				// the callback is a string like 'functionName' or 'Class::method'
+				if (count($rule['params']) === 1) {
+					$ruleId .= $rule['params'][0];
+				// the callback is an array like array('ClassName', 'staticMethod')
+				} elseif (count($rule['params']) === 2 and is_string($rule['params'][0])) {
+					$ruleId .= $rule['params'][0] . ':' . $rule['params'][1];
+				// the callback is an array like array($object, 'method')	
+				} elseif (count($rule['params']) === 2 and is_object($rule['params'][0])) {
+					$ruleId .= get_class($rule['params'][0]) . '->' . $rule['params'][1];
+				}
+			break;
+		}
+		return $ruleId;
 	}
 
 	function addCondition($name, $function) {
@@ -43,6 +70,34 @@ class Validator {
 	}
 
 	function add($selector, $rule, $params = null, $message = null, $condition = null) {
+		if (is_array($rule)) {
+			foreach ($rule as $singleRule) {
+				array_unshift($singleRule, $selector);
+				call_user_func_array(array($this, 'add'), $singleRule);
+			}
+			return $this;
+		}
+		if (is_string($rule)) {
+			// rule was supplied like this 'required | email'
+			if (strpos($rule, ' | ') !== false) {
+				return $this->add($selector, explode(' | ', $rule));
+			}
+			// rule was supplied like this 'if(condition)length[2,10]error'
+			if (strpos($rule, '[') !== false or strpos($rule, '(') !== false) {
+				list($rule, $params, $message, $condition) = $this->parseRule($rule);
+			}
+		}
+		$rule = array(
+			'name'		=> $rule,
+			'params'	=> $params,
+			'message'	=> $this->compileMessage($message),
+			'condition' => $condition
+		);
+		$ruleId = $this->getRuleId($rule);
+		if (!array_key_exists($selector, $this->rules)) {
+			$this->rules[$selector] = array();
+		}
+		$this->rules[$selector][$ruleId] = $rule;
 		return $this;
 	}
 
@@ -52,11 +107,12 @@ class Validator {
 	}
 
 	function validate($data) {
-
+		// reset messages
+		$this->messages = array();
 	}
 
 	function validateItem($data, $selector) {
-		
+
 	}
 
 	function getMessages() {
