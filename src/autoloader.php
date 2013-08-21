4e3ac9535227e2672859680ced14e20a141948ef
Florin Miu <florin_miu@yahoo.com>
2013-08-21 20:36:11 +0300

    Improved the autoloader

--- src/autoloader.php
+++ src/autoloader.php
@@ -8,6 +8,10 @@ spl_autoload_register(function(
 	if (strpos($class, __NAMESPACE__ . '\\') !== 0) {
 		return false;
 	}
-	return include (__DIR__ . substr($class, strlen(__NAMESPACE__)) .  '.php');
-} );
+	$file = __DIR__ . substr($class, strlen(__NAMESPACE__)) .  '.php';
+	if (file_exists($file)) {
+		return include_once($file);
+	}
+	return false;
+});
 
