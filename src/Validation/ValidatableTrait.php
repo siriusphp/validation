5b8e1ba9df0970b20fd7ba1e0888849dba6a402c
Florin Miu <florin_miu@yahoo.com>
2013-08-25 18:49:16 +0300

    Created the ValidatableInterface and ValidatableTrait. No tests for the trait

---
+++ src/Validation/ValidatableTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sirius\Validation;
+
+use Sirius\Validation\Validator;
+
+trait ValidatableTrait {
+    protected $validator;
+
+    function setValidator($validator) {
+        if (!$validator instanceof Validator) {
+            throw new \InvalidArgumentException('The $validator argument is not a proper Validator object');
+        }
+        $this->validator = $validator;
+        return $this;
+    }
+
+    function getValidator($validator) {
+        if (!$this->validator) {
+            $this->validator = new Validator();
+        }
+        return $this->validator;
+    }
+
+    function isValid() {
+        if (!method_exists($this, 'toArray')) {
+            throw new \BadMethodCallException('Object must have the "toArray" method to be able call "isValid"');
+        }
+        return $this->getValidator()->validate($this->toArray());
+    }
+}
