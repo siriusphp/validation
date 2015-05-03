e2df4687c5af98eb96cd7c2a3bec00b3b7401066
Adrian Miu <adrian@adrianmiu.ro>
2015-05-03 20:21:32 +0300

    Added the "order lines" scenario to the complex test

--- tests/src/ComplexTest.php
+++ tests/src/ComplexTest.php
@@ -15,7 +15,10 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
             ->add('password', 'required')
             ->add('password_confirm', 'required | match(item=password)')
             ->add('feedback', 'requiredwith(item=agree_to_provide_feedback)')
-            ->add('birthday', 'requiredwhen', array('item' => 'email_confirm', 'rule' => 'Email'));
+            ->add('birthday', 'requiredwhen', array('item' => 'email_confirm', 'rule' => 'Email'))
+            // the lines below don't match the example but that's ok,
+            // the individual rules have tests
+            ->add('lines[*][price]', 'requiredwith(item=lines[*][quantity])');
     }
 
     function notestWithCorrectData()
@@ -27,7 +30,10 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
             'password_confirm' => '1234',
             'agree_to_provide_feedback' => true,
             'feedback' => 'This is great!',
-            'birthday' => '1980-01-01'
+            'birthday' => '1980-01-01',
+            'lines' => array(
+                array('quantity' => 10, 'price' => 20)
+            )
         );
         $this->assertTrue($this->validator->validate($data));
     }
@@ -39,15 +45,17 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
             'password' => '1234',
             'password_confirm' => '123456',
             'agree_to_provide_feedback' => true,
+            'lines' => array(
+                array('quantity' => 10, 'price' => null)
+            )
         );
         $this->validator->validate($data);
         $messages = $this->validator->getMessages();
-        //print_r($this->validator->getMessages());
 
-        #$this->assertEquals('This field is required', (string)$messages['email'][0]);
-        #$this->assertEquals('This input must match password', (string)$messages['email'][0]);
-        #$this->assertEquals('This field is required', (string)$messages['feedback'][0]);
-
+        $this->assertEquals('This field is required', (string)$messages['email'][0]);
+        $this->assertEquals('This input does not match password', (string)$messages['password_confirm'][0]);
+        $this->assertEquals('This field is required', (string)$messages['feedback'][0]);
+        $this->assertEquals('This field is required', (string)$messages['lines[0][price]'][0]);
     }
 
 }
